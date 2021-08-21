2d336ae3b3759fce34781b9be0e54155878e85f7
Hai Nguyen <hai@ahijsc.com>
2021-08-21 11:58:24 +0700

    add livehelperchat.js

---
+++ js/livehelperchat.js
@@ -0,0 +1,7 @@
+var LHC_API = LHC_API||{};
+LHC_API.args = {mode:'widget',lhc_base_url:'//quangcaoviet.livehelperchat.com/',wheight:450,wwidth:350,pheight:520,pwidth:500,leaveamessage:true,check_messages:false,lang:'eng/'};
+(function() {
+var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
+var date = new Date();po.src = '//quangcaoviet.livehelperchat.com/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
