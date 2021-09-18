bcd71e9ed5c52a092e97f8e15d17ac193bc720bf
Hai Nguyen <hai@ahijsc.com>
2021-09-18 10:05:45 +0700

    new trial qcv01

--- js/quangcaoviet/livehelperchat.js
+++ js/quangcaoviet/livehelperchat.js
@@ -1,7 +1,7 @@
 var LHC_API = LHC_API||{};
-LHC_API.args = {mode:'widget',lhc_base_url:'//quangcaoviet.livehelperchat.com/',wheight:450,wwidth:350,pheight:520,pwidth:500,leaveamessage:true,check_messages:false,lang:'eng/'};
+LHC_API.args = {mode:'widget',lhc_base_url:'//qcv01.livehelperchat.com/',wheight:450,wwidth:350,pheight:520,pwidth:500,leaveamessage:true,check_messages:true};
 (function() {
-var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
-var date = new Date();po.src = '//quangcaoviet.livehelperchat.com/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    var date = new Date();po.src = '//qcv01.livehelperchat.com/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 })();
